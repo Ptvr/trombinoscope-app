439a7d6852d930bb1d0c4cd0f5b8a25dd99c4451
Pauline Tavernier <91745512+Ptvr@users.noreply.github.com>
2025-07-20 17:12:54 +0200

    Update config.js

--- AwesomeProject/config.js
+++ AwesomeProject/config.js
@@ -4,14 +4,14 @@ import "firebase/compat/firestore";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCSQg43krfHd-cW-2YUrzAaOfTOi8mvqjg",
-    authDomain: "epitech-widgetech.firebaseapp.com",
-    projectId: "epitech-widgetech",
-    storageBucket: "epitech-widgetech.appspot.com",
-    messagingSenderId: "910563011847",
-    appId: "1:910563011847:web:8bc5a78f8a084fb077e9a2",
-    measurementId: "G-7QPNDYM3EJ"
-  };
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
 
 if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
